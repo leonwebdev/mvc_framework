8f3321fb6764ca635d57ef73b740a578e935603e
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-06-16 10:43:20 -0500

    auth ui done

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+ ---------  Normal User Routes -------------------------------------------
+ */
+
 Route::get('/phones/index', "App\Http\Controllers\PhonesController@index");
 Route::get('/phones/{id}', "App\Http\Controllers\PhonesController@show");
 Route::get('/', function () {
     $title = 'Home';
     return view('home', compact('title'));
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+/*
+ ---------  Admin User Routes -------------------------------------------
+ */
+
+Route::get('/admin', "App\Http\Controllers\Admin\PhonesController@index");
+Route::get('/admin/create', "App\Http\Controllers\Admin\PhonesController@create");
+Route::post('/admin', "App\Http\Controllers\Admin\PhonesController@store");
+Route::get('/admin/edit/{id}', "App\Http\Controllers\Admin\PhonesController@edit")->name('edit');
+Route::put('/admin/{id}', "App\Http\Controllers\Admin\PhonesController@update")->name('update');
+Route::delete('/admin/{id}', "App\Http\Controllers\Admin\PhonesController@destroy");
